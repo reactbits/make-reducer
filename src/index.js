d3f5657b7f0aa0e1ce0cfe71b96fb89ea52c3252
sergeyt <stodyshev@gmail.com>
2016-01-20 11:06:28 +0600

    exposed makeActionCreator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import snakeCase from 'snake-case';
 
-function identity(t) {
-	return t;
-}
+function identity(t) { return t; }
 
 /**
  * Creates reducer function that returns the next state tree, given
@@ -12,7 +10,7 @@ function identity(t) {
  * @param {object} [handlers] The handler map to register.
  * @param {string} [actionTypePrefix] The prefix for all actions registered in this reducer.
  *
- * @returns {function} A reducer function to use with Redux store.
+ * @return {function} A reducer function to use with Redux store.
  */
 export default function makeReducer(initialState, handlers = {}, actionTypePrefix = '') {
 	const transitions = {};
@@ -26,45 +24,19 @@ export default function makeReducer(initialState, handlers = {}, actionTypePrefi
 	};
 
 	/**
-	 * Registers transition function to be used to handle specified action type.
-	 *
-	 * @param {any} [type] Specifies action type. It can be a named function where
-	 * name of function specifies action type and the function itself is used to handle
-	 * this action type.
-	 *
-	 * @param {function} [transition] (optional) A function to handle the action type.
-	 *
-	 * @param {function} [payloadReducer] (optional) A function to transform multiple arguments as a unique payload.
-	 *
+	 * Makes function to create action.
+	 * @param  {string} [type] Action type.
+	 * @param  {function} [payloadReducer] (optional) A function to transform multiple arguments as a unique payload.
 	 * @param {function} [metaCreator] (optional) A function to transform multiple arguments as a unique metadata object.
-	 *
-	 * @returns {function} A function to create action (aka action creator).
+	 * @return {function} A function to create action (aka action creator).
 	 */
-	reducer.on = function on(type, transition, payloadReducer = identity, metaReducer) {
-		if (typeof type === 'function') {
-			transition = type;
-			if (!transition.name) {
-				throw new Error('unexpected anonymous transition function');
-			}
-			type = snakeCase(transition.name).toUpperCase();
-		}
-
-		if (!type) {
-			throw new Error('action type is not defined');
-		}
-
-		if (typeof transition !== 'function') {
-			throw new Error('transition is not a function');
-		}
-
-		type = actionTypePrefix + type;
+	function makeActionCreator(type, payloadReducer = identity, metaReducer) {
+		const actionType = actionTypePrefix + type;
 		const hasMeta = typeof metaReducer === 'function';
-
-		transitions[type] = transition;
 
 		return (...args) => {
 			const action = {
-				type,
+				type: actionType,
 				payload: payloadReducer(...args),
 			};
 
@@ -79,6 +51,45 @@ export default function makeReducer(initialState, handlers = {}, actionTypePrefi
 
 			return action;
 		};
+	}
+
+	reducer.makeActionCreator = makeActionCreator;
+
+	/**
+	 * Registers transition function to be used to handle specified action type.
+	 *
+	 * @param {any} [type] Specifies action type. It can be a named function where
+	 * name of function specifies action type and the function itself is used to handle
+	 * this action type.
+	 *
+	 * @param {function} [transition] (optional) A function to handle the action type.
+	 * @param {function} [payloadReducer] (optional) A function to transform multiple arguments as a unique payload.
+	 * @param {function} [metaCreator] (optional) A function to transform multiple arguments as a unique metadata object.
+	 *
+	 * @return {function} A function to create action (aka action creator).
+	 */
+	reducer.on = function on(type, transition, payloadReducer = identity, metaReducer) {
+		let handler = transition;
+		let actionType = type;
+		if (typeof type === 'function') {
+			handler = type;
+			if (!handler.name) {
+				throw new Error('unexpected anonymous transition function');
+			}
+			actionType = snakeCase(handler.name).toUpperCase();
+		}
+
+		if (!actionType) {
+			throw new Error('action type is not defined');
+		}
+
+		if (typeof handler !== 'function') {
+			throw new Error('transition is not a function');
+		}
+
+		transitions[actionTypePrefix + actionType] = handler;
+
+		return makeActionCreator(actionType, payloadReducer, metaReducer);
 	};
 
 	// TODO remove or deprecate this API
